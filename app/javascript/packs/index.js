a416e33b346a8c7db18af27df9a5a334f5bc2e72
maxsean <xsma1990@gmail.com>
2018-01-03 12:59:38 -0500

    added enzyme test suite

---
+++ app/javascript/packs/index.js
@@ -0,0 +1,7 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import YourAppName from '../react/src/YourAppName';
+
+document.addEventListener('DOMContentLoaded', () => {
+  ReactDOM.render(<YourAppName />, document.getElementById('app'));
+})
---
+++ app/javascript/react/test/AppTest.js
@@ -0,0 +1,17 @@
+app/javascript/react/src/AppTest.js
+import App from '../src/App';
+import React from 'react'
+import { mount } from 'enzyme';
+import jasmineEnzyme from 'jasmine-enzyme';
+
+describe('A test for App', () => {
+  let wrapper;
+
+  beforeEach(() => {
+    wrapper = mount(<App />)
+  })
+
+  it('should pass', () => {
+    expect(wrapper.find('h1').text()).toEqual("Hello World")
+  })
+})
