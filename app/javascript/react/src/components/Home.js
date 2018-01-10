bf5eb332391530066c6c92cf6e5d1712e4f5d39b
maxsean <xsma1990@gmail.com>
2018-01-10 12:41:24 -0500

    added bubbles component

--- app/javascript/react/src/components/Home.js
+++ app/javascript/react/src/components/Home.js
@@ -3,7 +3,17 @@ import React from 'react'
 const Home = (props) => {
   return(
     <div>
-      Home
+      <div>
+        <ul className="bubbles">
+          <li><img style={{width:'100%',height:'100%'}} src={require("tech/ruby.png")}/></li>
+          <li><img style={{width:'100%',height:'100%'}} src={require("tech/rails.png")}/></li>
+          <li><img style={{width:'100%',height:'100%'}} src={require("tech/react.png")}/></li>
+          <li><img style={{width:'100%',height:'80%'}} src={require("tech/postgres.svg")}/></li>
+          <li><img style={{width:'100%',height:'100%'}} src={require("tech/d3.png")}/></li>
+          <li><img style={{width:'100%',height:'100%'}} src={require("tech/python.png")}/></li>
+        </ul>
+
+      </div>
     </div>
   )
 }
