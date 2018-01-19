51f78fece50380e47fe8a3556a76c4075394916f
maxsean <xsma1990@gmail.com>
2018-01-19 12:29:47 -0500

    fixed field components

--- app/javascript/react/src/components/TextInputField.js
+++ app/javascript/react/src/components/TextInputField.js
@@ -3,12 +3,12 @@ import React from 'react';
 // used in form containers
 const TextInputField = props => {
   return(
-    <label style={{fontSize:"16px"}} onChange={props.handleChange}>{props.label}
+    <label style={{fontSize:"16px"}} onChange={props.onChange}>{props.label}
     <input style={{width: "100%", height: "24px"}}
       id={props.name}
       name={props.name}
       type={props.type}
-      value={props.content}
+      value={props.value}
     />
     </label>
   )
