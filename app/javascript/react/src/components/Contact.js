51810157d7769444521423f1b3b91cbb2e2bfa07
maxsean <xsma1990@gmail.com>
2018-01-19 12:29:23 -0500

    added form validations

--- app/javascript/react/src/components/Contact.js
+++ app/javascript/react/src/components/Contact.js
@@ -1,45 +1,102 @@
 import React from 'react'
 import TextInputField from './TextInputField'
 import TextAreaField from './TextAreaField'
+import FormErrors from './FormErrors'
 
 class Contact extends React.Component {
   constructor(props) {
     super(props)
     this.state = {
-      username: "",
+      name: " ",
       contact: "",
-      message: "",
+      message: " ",
+      formErrors: {
+        name: '',
+        contact: '',
+        message: ''
+      },
+      nameValid: false,
+      contactValid: false,
+      messageValid: false,
+      formValid: false,
       success: false
     }
     this.handleSubmit = this.handleSubmit.bind(this)
     this.handleChange = this.handleChange.bind(this)
     this.clearForm = this.clearForm.bind(this)
     this.postMessage = this.postMessage.bind(this)
+    this.validateField = this.validateField.bind(this)
+    this.validateForm = this.validateForm.bind(this)
   }
 
   clearForm() {
     this.setState({
-      username: "",
+      name: "",
       contact: "",
-      message: ""
+      message: "",
+      formErrors: {
+        name: '',
+        contact: '',
+        message: ''
+      },
+      nameValid: false,
+      contactValid: false,
+      messageValid: false,
+      formValid: false
     })
   }
 
   handleSubmit(event) {
     event.preventDefault()
-    let formPayload = {
-      username: this.state.username,
-      contact: this.state.contact,
-      message: this.state.message
+    if (this.state.formValid) {
+      let formPayload = {
+        name: this.state.name,
+        contact: this.state.contact,
+        message: this.state.message
+      }
+      this.postMessage(formPayload)
+      this.clearForm()
     }
-    this.postMessage(formPayload)
-    this.clearForm()
   }
 
   handleChange(event) {
     let name = event.target.name
     let value = event.target.value
     this.setState({ [name]: value })
+    this.validateField(name, value)
+  }
+
+  validateField(fieldName, value) {
+    let fieldValidationErrors = this.state.formErrors
+    let nameValid = this.state.nameValid
+    let contactValid = this.state.contactValid
+    let messageValid = this.state.messageValid
+
+    switch(fieldName) {
+      case 'name':
+        nameValid = !value.match(/^\s+$/);
+        fieldValidationErrors["name"] = nameValid ? '' : ' cannot be blank';
+        break;
+      case 'contact':
+        contactValid = value.match(/^([\w.%+-]+)@([\w-]+\.)+([\w]{2,})$/i);
+        fieldValidationErrors["contact"] = contactValid ? '' : ' is invalid';
+        break;
+      case 'message':
+        messageValid = !value.match(/^\s+$/);
+        fieldValidationErrors["message"] = messageValid ? '' : ' cannot be blank';
+        break;
+    }
+    this.setState({
+      formErrors: fieldValidationErrors,
+      nameValid: nameValid,
+      contactValid: contactValid,
+      messageValid: messageValid
+    })
+    this.validateForm()
+  }
+
+  validateForm() {
+    this.setState({formValid: this.state.nameValid && this.state.contactValid && this.state.messageValid})
   }
 
   postMessage(formPayload) {
@@ -62,29 +119,44 @@ class Contact extends React.Component {
   }
 
   render() {
+    let success;
+    if (this.state.success) {
+      success =
+      <p>
+        I'll get back to you as soon as possible!
+      </p>
+    }
     return(
       <div>
         <div className="contact-header">
           <h2>Leave a Message!</h2>
         </div>
         <form className="contact-form" onSubmit={this.handleSubmit}>
+          <div className="errors">
+            <FormErrors formErrors={this.state.formErrors} />
+          </div>
+          <div className="success">
+            {success}
+          </div>
           <TextInputField
-            content={this.state.username}
+            value={this.state.name}
             label="Name: "
-            name="username"
-            handleChange={this.handleChange}
+            name="name"
+            onChange={this.handleChange}
+            type="text"
           />
           <TextInputField
-            content={this.state.contact}
-            label="Email: "
+            value={this.state.contact}
+            label="Contact: "
             name="contact"
-            handleChange={this.handleChange}
+            onChange={this.handleChange}
+            type="text"
           />
           <TextAreaField
-            content={this.state.message}
+            value={this.state.message}
             label="Message: "
             name="message"
-            handleChange={this.handleChange}
+            onChange={this.handleChange}
           />
           <div className='button-group'>
             <input className='button' type='submit' value='Submit' />
