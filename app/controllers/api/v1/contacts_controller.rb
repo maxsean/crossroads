c2b402cf69002ad9306a182d3e9ca7832949490f
maxsean <xsma1990@gmail.com>
2018-01-22 13:20:57 -0500

    remove pry

--- app/controllers/api/v1/contacts_controller.rb
+++ app/controllers/api/v1/contacts_controller.rb
@@ -1,7 +1,6 @@
 class Api::V1::ContactsController < Api::V1::ApiController
   def create
     body = JSON.parse(request.body.read)
-    binding.pry
     UserMailer.contact_message(body).deliver
     render json: {success: true}
   end
