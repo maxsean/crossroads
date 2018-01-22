8e0671dd59279f9688cbf55fbd73a2e63ec80dd1
maxsean <xsma1990@gmail.com>
2018-01-22 12:43:14 -0500

    fixed styling and cleanup

--- app/javascript/react/src/components/Home.js
+++ app/javascript/react/src/components/Home.js
@@ -4,19 +4,14 @@ import Blogs from '../containers/Blogs'
 const Home = (props) => {
   return(
     <div>
-      <div>
-        <ul className="bubbles">
-          <li><img style={{width:'100%',height:'100%'}} src={require("tech/ruby.png")}/></li>
-          <li><img style={{width:'100%',height:'100%'}} src={require("tech/rails.png")}/></li>
-          <li><img style={{width:'100%',height:'100%'}} src={require("tech/react.png")}/></li>
-          <li><img style={{width:'100%',height:'80%'}} src={require("tech/postgres.svg")}/></li>
-          <li><img style={{width:'100%',height:'100%'}} src={require("tech/d3.png")}/></li>
-          <li><img style={{width:'100%',height:'100%'}} src={require("tech/python.png")}/></li>
-          <li><img style={{width:'100%',height:'100%'}} src={require("tech/javascript.png")}/></li>
-        </ul>
-
-      </div>
-      <div className="home-tile">
+      <div
+        className="home-tile"
+        style={{
+          backgroundColor:"#373737",
+          color:"#C0B283",
+          textAlign:"center"
+        }}
+        >
           <p>
             My name is Sean and I'm a web developer. I enjoy finding elegant solutions to challenging problems and thrive in highly collaborative environments with multi-disciplinary teams.
           </p>
